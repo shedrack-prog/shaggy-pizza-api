de3ed10b6485c2d425ac9a5dd540d393a1af0ac2
Shedrach Tobiloba <usheddy07@gmail.com>
2024-07-04 20:11:58 +0100

    safeguading stripe_secret_key

--- utils/stripe.js
+++ utils/stripe.js
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = Stripe(
-  'sk_test_51Ol3AFC1Fucuq0dJPHd6o3w5NWVJeBgaKTkFhuTQ1NNe2U1eTDQE18TfqJEqD8YTfkqOzHsqbflz9pihElNpFiOJ00Kqv8Cuda',
+  'sk_test_51Ol3AFC1Fucuq0dJPHd6o3w5NWVJeBgaKTkFhuTQ1NNe2U1eTDQE18TfqJEqD8YTfkqOzHsqbflz9pihElNpFiOJ00Kqv8Cuda', //This is a test key but not accesible through .env-file...still working on it.
   {
     apiVersion: '2023-10-16',
   }
